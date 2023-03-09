7e320e6e2a833cb748c46d310c1ebdabc249b17a
vitalii Krasnorutskyi <krasnorutskyivitalyi@gmail.com>
2023-03-09 15:46:20 +0000

    Created recipes, ingredients and images entities.

---
+++ RecipesManagerApi.Domain/Entities/Recipe.cs
@@ -0,0 +1,31 @@
+using MongoDB.Bson;
+using RecipesManagerApi.Domain.Common;
+
+namespace RecipesManagerApi.Domain.Entities;
+
+public class Recipe : EntityBase
+{
+    public String Name { get; set; }
+
+    public Object? Thumbnail { get; set; }
+
+    public String? Text { get; set; }
+
+    public List<Ingredients>? Ingredients { get; set; }
+
+    public String? IngredientsText { get; set; }
+
+    public List<Category> Categories { get; set; }
+
+    public int? Calories { get; set; }
+
+    public int? ServingsCount { get; set; }
+
+    public bool IsPublic { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public ObjectId? LastModifiedById { get; set; }
+
+    public DateTime? LastModifiedDateUtc { get; set; }
+}

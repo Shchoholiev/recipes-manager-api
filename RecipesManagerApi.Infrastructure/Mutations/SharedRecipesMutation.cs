10c67d6e19166c2638d34c6ee77bf0c19064b9f5
Mykhailo <mihail.beloded.work@gmail.com>
2023-04-16 22:25:27 +0300

    RM-13 mutation and queries added

---
+++ RecipesManagerApi.Infrastructure/Mutations/SharedRecipesMutation.cs
@@ -0,0 +1,16 @@
+﻿using RecipesManagerApi.Application.IServices;
+using RecipesManagerApi.Application.Models;
+
+namespace RecipesManagerApi.Infrastructure.Mutations;
+
+[ExtendObjectType(OperationTypeNames.Mutation)]
+public class SharedRecipesMutation
+{
+    public Task AddSharedRecipeAsync(SharedRecipeDto dto, CancellationToken cancellationToken,
+    [Service] ISharedRecipesService recipesService)
+    => recipesService.AddSharedRecipeAsync(dto, cancellationToken);
+
+    public Task UpdateSharedRecipeAsync(SharedRecipeDto dto, CancellationToken cancellationToken,
+    [Service] ISharedRecipesService recipesService)
+    => recipesService.UpdateSharedRecipeAsync(dto, cancellationToken);
+}
---
+++ RecipesManagerApi.Infrastructure/Queries/SharedRecipesQuery.cs
@@ -0,0 +1,16 @@
+﻿using RecipesManagerApi.Application.IServices;
+using RecipesManagerApi.Application.Models;
+
+namespace RecipesManagerApi.Infrastructure.Queries;
+
+[ExtendObjectType(OperationTypeNames.Query)]
+public class SharedRecipesQuery
+{
+    public Task<SharedRecipeDto> AccessSharedRecipeAsync(SharedRecipeDto dto, CancellationToken cancellationToken,
+    [Service] ISharedRecipesService recipesService)
+    => recipesService.AccessSharedRecipeAsync(dto, cancellationToken);
+
+    public Task<SharedRecipeDto> GetSharedRecipeAsync(string id, CancellationToken cancellationToken,
+    [Service] ISharedRecipesService recipesService)
+    => recipesService.GetSharedRecipeAsync(id, cancellationToken);
+}

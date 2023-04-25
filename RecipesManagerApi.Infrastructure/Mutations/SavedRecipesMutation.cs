d73b0515f63642036ed7899ab836dd74c7120ae5
vitalii Krasnorutskyi <krasnorutskyivitalyi@gmail.com>
2023-04-25 20:39:09 +0200

    RM-40 adjusted according to commentaries

--- RecipesManagerApi.Infrastructure/Mutations/SavedRecipesMutation.cs
+++ RecipesManagerApi.Infrastructure/Mutations/SavedRecipesMutation.cs
@@ -2,16 +2,19 @@
 using RecipesManagerApi.Application.Models;
 using RecipesManagerApi.Application.Models.CreateDtos;
 using RecipesManagerApi.Application.Models.Operations;
+using HotChocolate.Authorization;
 
 namespace RecipesManagerApi.Infrastructure.Mutations;
 
 [ExtendObjectType(OperationTypeNames.Mutation)]
 public class SavedRecipesMutation
 {
+    [Authorize]
     public Task<SavedRecipeDto> AddSavedRecipeAsync(SavedRecipeCreateDto dto, CancellationToken cancellationToken,
      [Service] ISavedRecipesService recipesService)
      => recipesService.AddSavedRecipeAsync(dto, cancellationToken);
 
+    [Authorize]
     public Task<OperationDetails> DeleteSavedRecipeAsync(SavedRecipeDto dto, CancellationToken cancellationToken,
     [Service] ISavedRecipesService recipesService)
     => recipesService.DeleteSavedRecipeAsync(dto, cancellationToken);

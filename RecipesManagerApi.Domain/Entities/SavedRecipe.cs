70d30310dca25f3eee0401515fe43d64e84f996f
vitalii Krasnorutskyi <krasnorutskyivitalyi@gmail.com>
2023-05-01 14:04:46 +0200

    RM-65 added exception for unathorized update, removed user id from saved recipe, delete proceeds by id, update checks for author, updated queries and mutations for saved recipes and subscriptions

--- RecipesManagerApi.Domain/Entities/SavedRecipe.cs
+++ RecipesManagerApi.Domain/Entities/SavedRecipe.cs
@@ -4,9 +4,7 @@ using MongoDB.Bson;
 namespace RecipesManagerApi.Domain.Entities;
 
 public class SavedRecipe : EntityBase
-{
-    public ObjectId UserId { get; set; }
-
+{ 
     public ObjectId RecipeId { get; set; }
 
     public bool IsDeleted { get; set; }

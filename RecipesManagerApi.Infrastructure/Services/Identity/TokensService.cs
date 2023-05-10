2a2bce79108348e1d820be7b9650eb2def30ddc9
vitalii Krasnorutskyi <krasnorutskyivitalyi@gmail.com>
2023-05-10 16:20:27 +0200

    Deleted dublicated repository

--- RecipesManagerApi.Infrastructure/Services/Identity/TokensService.cs
+++ RecipesManagerApi.Infrastructure/Services/Identity/TokensService.cs
@@ -115,7 +115,7 @@ public class TokensService : ITokensService
         var tokenOptions = new JwtSecurityToken(
             issuer: _configuration.GetValue<string>("JsonWebTokenKeys:ValidIssuer"),
             audience: _configuration.GetValue<string>("JsonWebTokenKeys:ValidAudience"),
-            expires: DateTime.UtcNow.AddMinutes(60),
+            expires: DateTime.UtcNow.AddMinutes(5),
             claims: claims,
             signingCredentials: signinCredentials
         );

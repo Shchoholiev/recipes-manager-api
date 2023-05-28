7437380e7e268c3b0d8312352d0721c662f1b028
Shchoholiev <sh.codework@gmail.com>
2023-05-28 19:28:05 +0000

    RM-106 used Azure package to read SSE from OpenAI

--- RecipesManagerApi.Infrastructure/Services/OpenAiService.cs
+++ RecipesManagerApi.Infrastructure/Services/OpenAiService.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -27,11 +29,19 @@ public class OpenAiService : IOpenAiService
 
     private readonly IOpenAiLogsService _openAiLogsService;
 
-    public OpenAiService(IHttpClientFactory httpClientFactory, IOpenAiLogsRepository openAiLogsRepository, IOpenAiLogsService openAiLogsService)
+    private readonly IConfiguration _configuration;
+
+    private readonly OpenAIClient _openAIClient;
+
+    public OpenAiService(IHttpClientFactory httpClientFactory, IOpenAiLogsRepository openAiLogsRepository, IOpenAiLogsService openAiLogsService, IConfiguration configuration)
     {
         _httpClient = httpClientFactory.CreateClient("OpenAiHttpClient");
         _openAiLogsRepository = openAiLogsRepository;
         _openAiLogsService = openAiLogsService;
+        _configuration = configuration;
+
+	    var openAiApiKey = _configuration.GetSection("OpenAi")?.GetValue<string>("ApiKey");
+        _openAIClient = new OpenAIClient(openAiApiKey, new OpenAIClientOptions());
     }
 
     public async Task<OpenAiResponse?> GetChatCompletion(ChatCompletionRequest chat, CancellationToken cancellationToken)
@@ -57,12 +67,11 @@ public class OpenAiService : IOpenAiService
         return response;
     }
 
-    public async IAsyncEnumerable<OpenAiResponse> GetChatCompletionStream(ChatCompletionRequest chat, CancellationToken cancellationToken)
+    public async IAsyncEnumerable<OpenAiResponse> GetChatCompletionStream(ChatCompletionsOptions chat, CancellationToken cancellationToken)
     {
-        chat.Stream = true;
         var jsonBody = JsonConvert.SerializeObject(chat, _jsonSettings);
         var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-
+        Console.WriteLine(jsonBody);
         var log = await _openAiLogsService.AddLogAsync(new OpenAiLogDto {
             Request = jsonBody,
             Response = null,
@@ -70,25 +79,30 @@ public class OpenAiService : IOpenAiService
             CreatedById = GlobalUser.Id.ToString() ?? ObjectId.Empty.ToString(),
         }, cancellationToken);
 
-        var httpResponse = await _httpClient.PostAsync("chat/completions", body, cancellationToken);
-
         var allData = string.Empty;
-        using var streamReader = new StreamReader(await httpResponse.Content.ReadAsStreamAsync(cancellationToken));
-        while (!streamReader.EndOfStream)
-        {
-            var line = await streamReader.ReadLineAsync();
-            allData += line + "\n\n";
-            if (string.IsNullOrEmpty(line)) continue;
-            
-            var json = line?.Substring(6, line.Length - 6);
-            if (json == "[DONE]") {
-                log.Response = allData;
-                Task.Run(() => _openAiLogsService.UpdateLogAsync(log, cancellationToken));
-                yield break;
-            }
 
-            var openAiResponse = JsonConvert.DeserializeObject<OpenAiResponse>(json, _jsonSettings);
-            yield return openAiResponse;
+        var response = await _openAIClient.GetChatCompletionsStreamingAsync(
+            deploymentOrModelName: "gpt-3.5-turbo",
+            chat,
+            cancellationToken);
+        using StreamingChatCompletions streamingChatCompletions = response.Value;
+
+        await foreach (var choice in streamingChatCompletions.GetChoicesStreaming(cancellationToken))
+        {
+            await foreach (var message in choice.GetMessageStreaming(cancellationToken))
+            {
+                allData += JsonConvert.SerializeObject(message)+ "\n\n";;
+                if (message.Content == null) {
+                    log.Response = allData;
+                    Task.Run(() => _openAiLogsService.UpdateLogAsync(log, cancellationToken));
+                }
+
+                var openAiResponse = new OpenAiResponse {
+                    Choices = new List<OpenAiChoice> { new OpenAiChoice { Delta = new OpenAiDelta { Content = message.Content } }}
+                };
+
+                yield return openAiResponse;
+            }
         }
     }
 }
